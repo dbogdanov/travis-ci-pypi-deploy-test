18053bb9d1403ad96eeb118daaa1c1a457a8c290
Jannis Leidel <jannis@leidel.info>
2018-01-19 15:42:41 +0100

    Initial commit.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="travis-ci-pypi-deploy-test",
+    description="A Python package to test the Travis-CI PyPI deploy.",
+    author="Jannis Leidel",
+    license="MIT license",
+    author_email="jannis@leidel.info",
+    url="https://github.com/jezdez/travis-ci-pypi-deploy-test",
+    version='0.1',
+    py_modules=['travis_ci_pypi_deploy_test'],
+)
